e9abd1904f29c04abaf6b35c66aa1ae10d46c452
Paul Kinsky <pkinsky@gmail.com>
2018-05-07 00:39:47 -0700

    working

--- terraform/aws-env.tf
+++ terraform/aws-env.tf
@@ -249,6 +249,19 @@ resource "aws_lb_target_group" "example" {
 }
 
 
+resource "aws_lb_target_group_attachment" "example-2" {
+  target_group_arn = "${aws_lb_target_group.example.arn}"
+  target_id        = "${aws_instance.example-2.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "example-1" {
+  target_group_arn = "${aws_lb_target_group.example.arn}"
+  target_id        = "${aws_instance.example-1.id}"
+  port             = 80
+}
+
+
 resource "aws_lb_listener" "http-example" {
   load_balancer_arn = "${aws_lb.example.arn}"
   port              = "80"
@@ -308,3 +321,38 @@ data "aws_vpc" "main" {
 data "aws_subnet_ids" "example" {
   vpc_id = "${var.vpc_id}"
 }
+
+
+# even more stuff (r53)
+
+
+resource "aws_route53_record" "www-hiki" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "www.hikikomorphism.com"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.example.dns_name}"
+    zone_id                = "${aws_lb.example.zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "hiki" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name    = "hikikomorphism.com"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.example.dns_name}"
+    zone_id                = "${aws_lb.example.zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "hikikomorphism.com"
+}
+
+
+
